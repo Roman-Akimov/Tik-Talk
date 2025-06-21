867517cebab7f974f944f8911509e21f1f68fd9d
Roman-Akimov <roma_akimov_12@mail.ru>
2025-06-21 21:57:51 +0400

    1.change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProfileCard} from './common-ui/profile-card/profile-card';
+import {Profile} from './data/interfaces/profile.interface';
+import {CommonModule} from '@angular/common';
+import { ProfileService } from './data/services/profile'; // сервис
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ProfileCard],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
+  profileService = inject(ProfileService)
+  profiles: Profile[] = []
+
+  constructor() {
+    this.profileService.getTestAccounts()
+      .subscribe(val => {
+        this.profiles = val;
+      })
+  }
   protected title = 'tik-talk';
 }
