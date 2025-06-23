e20e2d690e51178d711cb82918508cba6ec5b8b0
Roman-Akimov <roma_akimov_12@mail.ru>
2025-06-23 23:24:15 +0400

    3. Авторизация 1/2 часть

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,15 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ProfileCard} from './common-ui/profile-card/profile-card';
-import {Profile} from './data/interfaces/profile.interface';
+
 import {CommonModule} from '@angular/common';
-import { ProfileService } from './data/services/profile'; // сервис
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, ProfileCard],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
 export class App {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = [];
 
-  constructor() {
-    this.profileService.getTestAccounts()
-      .subscribe(val => {
-        this.profiles = val;
-      });
-  }
-  protected title = 'tik-talk';
 }
